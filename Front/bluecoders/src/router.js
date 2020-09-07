afb629fe7e12a1ac2c8916b809a5efc2b058508f
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-08 00:17:51 +0200

    [ADD] - vuerouter added

--- Front/bluecoders/src/router.js
+++
@@ -1,7 +0,0 @@
-const SignIn = { template: '<div>sign-in</div>' }
-const SignUp = { template: '<div>sign-up</div>' }
-
-const routes = [
-  { path: '/sign-in', component: SignIn },
-  { path: '/sign-up', component: SignUp }
-]
