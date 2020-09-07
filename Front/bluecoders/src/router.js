e6fa272b0597bb900d364f29632423698839b57c
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-08 00:13:57 +0200

    [ADD] - create a vuejs project

---
+++ Front/bluecoders/src/router.js
@@ -0,0 +1,7 @@
+const SignIn = { template: '<div>sign-in</div>' }
+const SignUp = { template: '<div>sign-up</div>' }
+
+const routes = [
+  { path: '/sign-in', component: SignIn },
+  { path: '/sign-up', component: SignUp }
+]
