9fb2d43e1c5eb468967153d806d2692813a963ac
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-07 16:14:41 +0200

    [MODIFIED] - Change the different get

--- Back/Server/server.js
+++ Back/Server/server.js
@@ -11,6 +11,18 @@ app.get("/user-create", (req, res) => {
  res.send("User created \n");
 });
 
+app.get("/user-delete", (req, res) => {
+    res.send("User deleted \n");
+});
+
+app.get("/user-modify", (req, res) => {
+    res.send("User modified \n");
+});
+
+app.get("/todo", (req, res) => {
+    res.send("todo list \n");
+});
+
 app.listen(PORT, function() {
     console.log(`Listening on ${PORT}`);
 });
