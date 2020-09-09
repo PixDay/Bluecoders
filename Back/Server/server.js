823405c661b5061e815d0d693a1b18e0fc53c105
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-09 16:53:00 +0200

    [MODIFIED] - server.js modified it can now create a todo with a user id, a title, a description and a default state

--- Back/Server/server.js
+++ Back/Server/server.js
@@ -3,8 +3,7 @@ const PORT = 4000;
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 const User = require("./model/User.model");
-const mongoDB = require("./src/connection");
-const connectDb = require('./src/connection');
+const Todo = require("./model/Todo.model");
 
 
 MongoClient.connect('mongodb://root:root@localhost:27017', {useUnifiedTopology: true}).then(client => {
@@ -16,6 +15,7 @@ MongoClient.connect('mongodb://root:root@localhost:27017', {useUnifiedTopology: 
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(bodyParser.json())
 
+    /* USERS */
     app.post("/connect", async (req, res) => {
         userCollection.findOne({}).then(result => {
             console.log(result)
@@ -37,6 +37,20 @@ MongoClient.connect('mongodb://root:root@localhost:27017', {useUnifiedTopology: 
     
     app.get("/usermodify", (req, res) => {
         res.send("User modified \n");
+    });
+
+    /* TODO */
+    app.get("/todolist", (req, res) => {
+        res.send("Todo list");
+    })
+
+    app.post("/todocreate", async (req, res) => {
+        const givenTodo = new Todo({id: req.body.id, title: req.body.title, description: req.body.description, state: "todo"});
+
+        todoCollection.insertOne(givenTodo).then(result => {
+            console.log(result)
+        }).catch(error => console.error(error));
+        res.redirect("/");
     });
 
     app.listen(PORT, function() {
