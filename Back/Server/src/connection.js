94789915da050b662994727df02dc47c6f2835ce
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-10 23:15:49 +0200

    [DELETE] - Remove useless code

--- Back/Server/src/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const mongoDB = 'mongodb://127.0.0.1/todolist'
-
-const connectDb = () => {
-    return mongoose.connect(mongoDB, {useUnifiedTopology: true, useNewUrlParser: true});
-};
-
-module.exports = connectDb;
