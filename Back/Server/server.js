1a1594cd0f2af76ca84c2efb68291487e0126979
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-10 22:57:19 +0200

    [MODIFIED] - Changing routes and token working

--- Back/Server/server.js
+++ Back/Server/server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const PORT = 4000;
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
+
 import {User, Todo} from "./model/Schemas.model";
 import * as bcrypt from "bcryptjs";
 import {readFileSync} from "fs";
@@ -19,7 +20,8 @@ MongoClient.connect('mongodb://root:root@localhost:27017', {useUnifiedTopology: 
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(bodyParser.json())
 
-    app.post('/login', (req, res) => {
+    /* USERS */
+    app.post('/login', async (req, res) => {
         const mail = req.body.mail;
         const password = req.body.password;
         const user = await userCollection.findOne({mail: mail});
@@ -39,6 +41,50 @@ MongoClient.connect('mongodb://root:root@localhost:27017', {useUnifiedTopology: 
         const user = new User({firstname: firstname, name: name, mail: mail, password: bcrypt.hashSync(password, saltRound)});
         await userCollection.save(user);
         res.status(200).send("User created successfully");
+    });
+
+    /* TODO */
+    app.post('/todo/create', async (req, res) => {
+        const title = req.body.title;
+        const description = req.body.description;
+        const token = jwt.verify(req.header.authorization, privateKey);
+        
+        const todo = new Todo({user: token.id, title: title, description: description, checked: false})
+        await todoCollection.save(todo);
+        res.status(200).send("Todo created successfully");
+    });
+
+    app.delete('/todo/delete', async (req, res) => {
+        const todoId = req.body._id;
+        const userId = jwt.verify(req.header.authorization, privateKey);
+
+        await todoCollection.findOneAndDelete({_id: todoId, user: userId});
+        res.status(200).send("Todo deleted");
+    });
+
+    app.post('/todo/update', async (req, res) => {
+        const todoId = req.body._id;
+        const title = req.body.title;
+        const description = req.body.description;
+        const checked = req.body.checked;
+        const userId = jwt.verify(req.header.authorization, privateKey);
+        const todo = new Todo({user: userId, title: title, description: description, checked: checked});
+
+        await todoCollection.findOneAndUpdate({_id: todoId, user: userId}, todo);
+        res.status(200).send("Todo deleted");
+    });
+
+    app.post('/todo/toggle/', async (req, res) => {
+        const todoId = req.body._id;
+        const todo = todoCollection.findOne({_id: todoId, user: jwt.verify(req.header.authorization, privateKey)});
+
+        await todoCollection.findOneAndUpdate({_id: todoId, user: jwt.verify(req.header.authorization, privateKey), checked: todo.checked});
+        res.status(200).send(checked.checked);
+    })
+
+    app.get('/todo', async (req, res) => {
+        await todoCollection.find({user: jwt.verify(req.header.authorization, privateKey)});
+        res.status(200).send("Todo gotten");
     });
 
     app.listen(PORT, function() {
