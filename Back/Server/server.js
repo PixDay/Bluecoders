a862fbcf3de07071366796cc9c58229e21896dcb
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-10 22:04:18 +0200

    [ADD] - jwt and bcrypt

--- Back/Server/server.js
+++ Back/Server/server.js
@@ -2,78 +2,44 @@ const app = require('express')();
 const PORT = 4000;
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
-const User = require("./model/User.model");
-const Todo = require("./model/Todo.model");
+import {User, Todo} from "./model/Schemas.model";
+import * as bcrypt from "bcryptjs";
+import {readFileSync} from "fs";
+import * as jwt from "jsonwebtoken";
 
+const privateKey = readFileSync(__dirname + "/.privatekey");
 
 MongoClient.connect('mongodb://root:root@localhost:27017', {useUnifiedTopology: true}).then(client => {
     console.log('Connected to Database')
     const db = client.db('todolist')
     const userCollection = db.collection('User');
     const todoCollection = db.collection('Todo');
+    const saltRound = 10;
 
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(bodyParser.json())
 
-    /* USERS */
-    app.post("/connect", async (req, res) => {
-        userCollection.findOne({}).then(result => {
-            console.log(result)
-        }).catch(error => console.error(error));
-    });
-    
-    app.post("/usercreate", async (req, res) => {
-        const givenUser = new User({firstname: req.body.firstname, name: req.body.name, password: req.body.password, mail: req.body.mail});
-
-        userCollection.insertOne(givenUser).then(result => {
-            console.log(result)
-        }).catch(error => console.error(error));
-        res.redirect("/");
-    });
-    
-    app.get("/userdelete", (req, res) => {
-        res.send("User deleted \n");
-    });
-    
-    app.get("/usermodify", (req, res) => {
-        res.send("User modified \n");
+    app.post('/login', (req, res) => {
+        const mail = req.body.mail;
+        const password = req.body.password;
+        const user = await userCollection.findOne({mail: mail});
+        
+        if (!user || !bcrypt.compareSync(password, user.password))
+            res.status(400).send("Unknown user");
+        const token = jwt.sign({id: user._id}, privateKey);
+        res.status(200).send(token);
     });
 
-    /* TODO */
-    app.get("/todolist", (req, res) => {
-        todoCollection.find({}, {title: 1, description: 1}).toArray().then(result => {
-            console.log(result);
-        });
-        res.send("Todolist");
-    })
+    app.post('/register', async (req, res) => {
+        const firstname = req.body.firstname;
+        const name = req.body.name;
+        const mail = req.body.mail;
+        const password = req.body.password;
 
-    app.post("/todocreate", async (req, res) => {
-        const givenTodo = new Todo({ reference: req.body.reference, title: req.body.title, description: req.body.description, state: "todo"});
-
-        todoCollection.insertOne(givenTodo).then(result => {
-            console.log(result)
-        }).catch(error => console.error(error));
-        res.redirect("/");
+        const user = new User({firstname: firstname, name: name, mail: mail, password: bcrypt.hashSync(password, saltRound)});
+        await userCollection.save(user);
+        res.status(200).send("User created successfully");
     });
-
-    app.post("/tododelete", async (req, res) => {
-        const givenTodo = new Todo({reference: req.body.reference, title: req.body.title, description: req.body.description, state: "todo"});
-
-        todoCollection.deleteOne(givenTodo).then(result => {
-            console.log(result);
-        }).catch(error => console.error(error));
-        res.redirect("/");
-    })
-
-    app.post("/todoupdate", async (req, res) => {
-        const givenTodo = new Todo({ title: req.body.title, description: req.body.description, state: "todo"});
-
-        console.log(givenTodo);
-        todoCollection.findOneAndUpdate({ title: "Bluecoders"}, givenTodo).then(result => {
-            console.log(result);
-        }).catch(error => console.error(error));
-        res.redirect("/");
-    })
 
     app.listen(PORT, function() {
         console.log(`Listening on ${PORT}`);
