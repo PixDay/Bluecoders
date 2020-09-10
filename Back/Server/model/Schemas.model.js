fc9e9982a9c9eef49de7b308cf805de002f39d8a
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-10 21:35:13 +0200

    [MODIFIED] - change models

---
+++ Back/Server/model/Schemas.model.js
@@ -0,0 +1,20 @@
+import * as mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstname: String,
+    name: String,
+    mail: {type: String, unique: true},
+    password: String
+});
+
+const todoSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "Todo"},
+    title: String,
+    description: String,
+    checked: Boolean
+});
+
+const User = mongoose.model("User", userSchema);
+const Todo = mongoose.model("Todo", todoSchema);
+
+export {User, Todo};
