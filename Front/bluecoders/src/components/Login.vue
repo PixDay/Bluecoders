bf9279becae39e066096e7079497ee70bdfff3c8
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-11 02:40:07 +0200

    [ADD] - Required input for loging in

--- Front/bluecoders/src/components/Login.vue
+++ Front/bluecoders/src/components/Login.vue
@@ -4,13 +4,13 @@
       <h3>Login</h3>
       <div class="form-group">
         <label>Email</label>
-        <input type="email" class="form-control form-control-lg" />
+        <input type="email" class="form-control form-control-lg" required/>
       </div>
       <div class="form-group">
         <label>Password</label>
-        <input type="password" class="form-control form-control-lg" />
+        <input type="password" class="form-control form-control-lg" required/>
       </div>
-      <button type="submit" class="btn btn-dark btn-lg btn-block"><router-link :to="{name: 'todolist'}" tag="white-link">Login</router-link></button>
+      <button type="submit" class="btn btn-success btn-lg btn-block"><router-link :to="{name: 'todolist'}" tag="white-link">Login</router-link></button>
       <button class="btn btn-primary btn-lg btn-block"><router-link :to="{name: 'signup'}" tag="white-link">Register</router-link></button>
     </form>
   </div>
