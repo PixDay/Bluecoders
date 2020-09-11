84400aadef83df2762a98a557971c7f32b710714
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-11 02:22:36 +0200

    [MODIFIED] - Add link-routed button to the vue

--- Front/bluecoders/src/components/Login.vue
+++ Front/bluecoders/src/components/Login.vue
@@ -1,18 +1,19 @@
 <template>
-    <div class="vue-tempalte">
-        <form>
-            <h3>Sign In</h3>
-            <div class="form-group">
-                <label>Email address</label>
-                <input type="email" class="form-control form-control-lg" />
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control form-control-lg" />
-            </div>
-            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
-        </form>
-    </div>
+  <div class="vue-tempalte">
+    <form>
+      <h3>Sign In</h3>
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control form-control-lg" />
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control form-control-lg" />
+      </div>
+      <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
+      <button class="btn btn-primary btn-lg btn-block"><router-link :to="{name: 'signup'}" tag="white-link">Register</router-link></button>
+    </form>
+  </div>
 </template>
 
 <script>
--- Front/bluecoders/src/components/Signup.vue
+++ Front/bluecoders/src/components/Signup.vue
@@ -1,22 +1,23 @@
 <template>
-    <div class="vue-tempalte">
-        <form>
-            <h3>Sign Up</h3>
-            <div class="form-group">
-                <label>Full Name</label>
-                <input type="text" class="form-control form-control-lg"/>
-            </div>
-            <div class="form-group">
-                <label>Email address</label>
-                <input type="email" class="form-control form-control-lg" />
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control form-control-lg" />
-            </div>
-            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign Up</button>
-        </form>
-    </div>
+  <div class="vue-tempalte">
+    <form>
+      <h3>Sign Up</h3>
+      <div class="form-group">
+        <label>Full Name</label>
+        <input type="text" class="form-control form-control-lg"/>
+      </div>
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control form-control-lg" />
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control form-control-lg" />
+      </div>
+      <button type="submit" class="btn btn-dark btn-lg btn-block">Sign Up</button>
+      <button class="btn btn-primary btn-lg btn-block"><router-link :to="{name: 'login'}" tag="white-link">Login</router-link></button>
+    </form>
+  </div>
 </template>
 
 <script>
