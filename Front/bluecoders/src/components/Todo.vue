eaa803f2eab5dbb710bdc913556fc639c10758f2
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-11 10:09:34 +0200

    [ADD] - Todo.vue content

--- Front/bluecoders/src/components/Todo.vue
+++ Front/bluecoders/src/components/Todo.vue
@@ -1,5 +1,18 @@
 <template>
-    <p>Tasks</p>
+    <div class="vue-tempalte">
+        <h2>Tasks</h2>
+        <div class="input-group">
+            <input type="text" class="form-control form-control-lg" placeholder="New task"/>
+            <span class="input-group-btn">
+                <button type="submit" class="btn btn-primary btn-lg"><i class="fa fa-plus"></i> Add</button>
+            </span>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-warning btn-lg widthbutton"><i class="fa fa-check"></i> Clear completed</button>
+        <br>
+        <br>
+        <button type="submit" class="btn btn-danger btn-lg widthbutton"><i class="fa fa-trash"></i> Clear all</button>
+    </div>
 </template>
 
 <script>
