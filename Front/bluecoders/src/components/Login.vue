f5097327004f9e8355461d0d899d5c6d0ecb6166
Adrien Colombier <adrien.colombier@epitech.eu>
2020-09-11 02:31:33 +0200

    [MODIFIED] - Changing the text

--- Front/bluecoders/src/components/Login.vue
+++ Front/bluecoders/src/components/Login.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="vue-tempalte">
     <form>
-      <h3>Sign In</h3>
+      <h3>Login</h3>
       <div class="form-group">
-        <label>Email address</label>
+        <label>Email</label>
         <input type="email" class="form-control form-control-lg" />
       </div>
       <div class="form-group">
         <label>Password</label>
         <input type="password" class="form-control form-control-lg" />
       </div>
-      <button type="submit" class="btn btn-dark btn-lg btn-block"><router-link :to="{name: 'todolist'}" tag="white-link">Sign In</router-link></button>
+      <button type="submit" class="btn btn-dark btn-lg btn-block"><router-link :to="{name: 'todolist'}" tag="white-link">Login</router-link></button>
       <button class="btn btn-primary btn-lg btn-block"><router-link :to="{name: 'signup'}" tag="white-link">Register</router-link></button>
     </form>
   </div>
